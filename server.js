fe7fa4a9d06b3d4851ba89c8ec32cb555bae5f81
Blake Schwartz <blakehschwartz@gmail.com>
2017-05-20 12:32:10 -0700

    ex-1 complete

--- server.js
+++ server.js
@@ -3,9 +3,21 @@
 const Hapi = require('hapi');
 
 // Create a Hapi server instance
-
+const server = new Hapi.Server();
 // Tell the server which port to listen on
-
+server.connection({port: 3001, host: 'localhost'});
 // Create a hello world route
-
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: (request, reply) => {
+        reply({message: 'Hello World!'});
+    }
+});
 // Start the server and log out the URI
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
